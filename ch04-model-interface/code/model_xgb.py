e724d73295cb7f6ecb7332cb2ae82b9a22fbf0bf
Yutaka Nishizaki <steelpipe75@gmail.com>
2025-04-07 21:02:04 +0900

    `ntree_limit=self.model.best_ntree_limit`エラー対応

--- ch04-model-interface/code/model_xgb.py
+++ ch04-model-interface/code/model_xgb.py
@@ -34,7 +34,8 @@ class ModelXGB(Model):
 
     def predict(self, te_x):
         dtest = xgb.DMatrix(te_x)
-        return self.model.predict(dtest, ntree_limit=self.model.best_ntree_limit)
+        # return self.model.predict(dtest, ntree_limit=self.model.best_ntree_limit)
+        return self.model.predict(dtest, iteration_range=(0, self.model.best_iteration + 1))
 
     def save_model(self):
         model_path = os.path.join('../model/model', f'{self.run_fold_name}.model')
